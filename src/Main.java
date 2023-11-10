28ee862fdedc9ddc5c9bddd9ed2dae7b409f9848
Borja <brb0004@alu.medac.es>
2023-11-10 11:12:16 +0100

    Comienzo de proyecto día 10/11

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Impresora impresora = new Impresora();
+        /* Impresora impresora = new Impresora();
         HiloPIM hiloPIM = new HiloPIM(impresora);
         HiloPAM hiloPAM = new HiloPAM(impresora);
 
         hiloPIM.start();
         hiloPAM.start();
-
+        */
         /* ImpresoraMonitor impresoraMonitor = new ImpresoraMonitor();
         HiloPIMMonitor hiloPIMMonitor = new HiloPIMMonitor(impresoraMonitor);
         HiloPAMMonitor hiloPAMMonitor = new HiloPAMMonitor(impresoraMonitor);
@@ -14,5 +14,12 @@ public class Main {
         hiloPIMMonitor.start();
         hiloPAMMonitor.start();
         */
+
+        ImpresoraSemaforo impresoraSemaforo = new ImpresoraSemaforo();
+        HiloPIMSemaforo hiloPIMSemaforo = new HiloPIMSemaforo(impresoraSemaforo);
+        HiloPAMSemaforo hiloPAMSemaforo = new HiloPAMSemaforo(impresoraSemaforo);
+
+        hiloPIMSemaforo.start();
+        hiloPAMSemaforo.start();
     }
 }
